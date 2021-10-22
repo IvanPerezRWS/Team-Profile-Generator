eb0f3b7d5279a848175f0e4daf8510284c8ee4f8
IvanPerezRWS <ivan.perez.rws@gmail.com>
2021-10-21 19:32:02 -0500

    Added Manager tests and .js

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,7 +1,24 @@
 
 
-const checkIfEqual = require('../lib/random.js')
+//name, id, email, getname(), getId() getEmail() getRole()
+// officenumber getRole -> return'Manager'
 
-test('checks if 10 is equal to 10', () => {
-    expect(checkIfEqual(10, 10)).toBe(true);
+const Manager = require('../lib/Manager');
+
+const managerTest = new Manager('Marcus', '8', 'MarcusLaminos@gmail.com', 'Manager', '123');
+
+test('Check name property', () => {
+    expect(managerTest.name).toBe('Marcus');
+});
+test('Check id property', () => {
+    expect(managerTest.id).toBe('8');
+});
+test('Check email property', () => {
+    expect(managerTest.email).toEqual(expect.stringContaining('@'));
+});
+test('Check role property', () => {
+    expect(managerTest.role).toBe('Manager');
+});
+test('Check room property', () => {
+    expect(managerTest.officeNumber).toBe('123');
 });
